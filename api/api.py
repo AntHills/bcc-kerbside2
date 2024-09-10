b675f88df08d0df452fe2e2fb67b7951fe5d513d
James W <jamesweber1994@gmail.com>
2024-09-10 12:37:50 +1000

    created script to get data, serving data on api, displaying on site

--- api/api.py
+++ api/api.py
@@ -1,6 +1,6 @@
 import time
 import sqlite3
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -15,9 +15,22 @@ def get_db_connection():
 def get_current_time():
   return {'time': time.time()}
 
-@app.route('/kerbside')
+@app.route('/api/kerbside', methods=['GET'])
 def get_kerbside_data():
   conn = get_db_connection()
-  kerbside_dates = conn.execute('SELECT * FROM kerbside').fetchall()
+  cur = conn.cursor()
+
+  cur.execute('SELECT * FROM kerbside')
+  rows = cur.fetchall()
+
+  data = [
+        {
+            'id': row[0],
+            'updated': row[1],
+            'suburb': row[2],
+            'kerbsideWeek': row[3]
+        } for row in rows
+    ]
   conn.close()
+  return jsonify(data)
   
--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,29 @@
 import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [currentTime, setCurrentTime] = useState(0);
+  const [kerbsideData, setKerbsideData] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/time")
+    fetch("http://localhost:5000/api/kerbside")
       .then((res) => res.json())
-      .then((data) => setCurrentTime(data.time));
+      .then((data) => setKerbsideData(data));
 
-    console.log(currentTime);
+    console.log(kerbsideData);
   }, []);
+
+  const tableRows = kerbsideData.map((k) => {
+    return (
+      <tr>
+        <th>{k.suburb}</th>
+        <th>{k.kerbsideWeek}</th>
+        <th>{k.updated}</th>
+      </tr>
+    );
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p>{currentTime}</p>
+      <table>{tableRows}</table>
     </>
   );
 }
